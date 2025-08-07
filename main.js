bf7f20a89332cb9cfc7a5ab527bfb791ff879267
GIOVANNA COELHO GILIO DE SA <24122381@dsn127m1249389.cfp127aluno.local>
2025-08-07 15:55:35 -0300

    front ok

--- main.js
+++ main.js
@@ -13,12 +13,15 @@ async function mostrarSlide (imagens, tempo = 3000){
     let inicio = 0
 
     const fotoAPI = document.getElementById('imagens')
-    const legendaAPI = document.getElementById('legenda')
+    const legendaAPI = document.getElementById('legenda')    
+    const dataAPI = document.getElementById('data')
+
 
     function mudarFoto( ){
         const foto =  imagens[inicio]
         fotoAPI.src = foto.imagem
         legendaAPI.textContent = foto.legenda
+        dataAPI.textContent = foto.data
 
         inicio++
         if (inicio >= imagens.length) {
